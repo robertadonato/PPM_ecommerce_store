631d2564e5ffcd58148683445a06ae9b7aa3bf78
roberta donato <129007908+robertadonato@users.noreply.github.com>
2025-06-24 16:01:47 +0200

    Update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        default=config('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
